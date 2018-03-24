48948675f9aec6718c90dfc2362f7adbca6e690a
Phil Forrence <prf8@pitt.edu>
2018-03-24 14:40:22 -0400

    Encoding grammar

--- Factorial.java
+++ Factorial.java
@@ -1,32 +1,18 @@
-//Phil Forrence
-class Factorial {
-
-      public static void main( String[] a ) {
-
-            System.out.println( new Fac().ComputeFac( 10 ) );
-
-      }
-
+class Factorial{
+    public static void main(String[] a){
+      System.out.println(new Fac().ComputeFac(10));
+    }
 }
 
- /*Phil Forrence*/
+class Fac {
 
-class Fac { 
-
-      public int ComputeFac( int num ) {
-
-            int num_aux;
-
-            if( num < 1 )
-
-                  num_aux = 1;
-
-            else
-
-                  num_aux = num * ( this.ComputeFac( num – 1 ) );
-
-            return num_aux;
-
-      }
+    public int ComputeFac(int num){
+      int num_aux ;
+      if (num < 1)
+          num_aux = 1 ;
+      else 
+          num_aux = num * (this.ComputeFac(num-1)) ;
+      return num_aux ;
+    }
 
 }
